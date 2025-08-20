80ff687f10aad9259ef984e7118954940e62a63e
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-08-20 12:22:13 +0700

    feat(reservations): enhance validation in CreateReservationDto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,7 +1,20 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
   startDate: Date;
+
+  @IsDate()
   endDate: Date;
-  invoiceId: string;
+
+  @IsString()
   userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 }
