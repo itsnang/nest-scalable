af9e3ad6c1e56fe35377f838b12c1f6170bf3dd2
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-08-20 12:24:58 +0700

    fix(reservations): add Type decorator for date fields

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,14 +1,14 @@
+import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
   @IsDate()
+  @Type(() => Date)
   startDate: Date;
 
   @IsDate()
+  @Type(() => Date)
   endDate: Date;
-
-  @IsString()
-  userId: string;
 
   @IsString()
   @IsNotEmpty()
