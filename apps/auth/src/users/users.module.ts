fb8a603b77cb61ee11c29d8fc8c189c9e3580359
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-08-20 15:05:31 +0700

    feat(auth): implement authentication module with user management

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { DatabaseModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserDocument, UserSchema } from './models/users.schema';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    MongooseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
+})
+export class UsersModule {}
