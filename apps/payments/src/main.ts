ae26c7f4c40cbd11d69d443a541600ed132f1b46
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-08-31 16:07:08 +0700

    feat(payments): add payments service to docker-compose and update nest-cli configuration

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { PaymentsModule } from './payments.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get<number>('PORT') ?? 3003,
+    },
+  });
+  await app.startAllMicroservices();
+}
+bootstrap();
---
+++ apps/payments/src/payments.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
