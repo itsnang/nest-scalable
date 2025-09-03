f00d50de1918b88fff6b779026a62038608a0c03
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-09-03 22:17:58 +0700

    feat(payments): add Stripe integration

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -11,6 +11,7 @@ import { PaymentsService } from './payments.service';
       isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
       }),
     }),
     LoggerModule,
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
+  private readonly = new Stripe(this.configService.get('STRIPE_SECRET_KEY'));
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
     return 'Hello World!';
   }
