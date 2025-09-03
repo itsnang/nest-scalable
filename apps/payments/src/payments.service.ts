b752f21cdbb97fdd476073aab613136d3fc93cba
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-09-03 22:44:32 +0700

    feat(payments): implement create charge functionality

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  private readonly = new Stripe(this.configService.get('STRIPE_SECRET_KEY'));
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+  );
   constructor(private readonly configService: ConfigService) {}
-  getHello(): string {
-    return 'Hello World!';
+
+  async createCharge(createChargeDto: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card: createChargeDto.card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: createChargeDto.amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+    return paymentIntent;
   }
 }
